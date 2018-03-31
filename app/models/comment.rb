99099fdff48455ad14568af9e8a03706043d1d63
Chris Henry <cdhenry@gmail.com>
2018-03-31 23:09:06 +0000

    automatically backed up by learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
-  accepts_nested_attributes_for :user, reject_if: proc { |attributes| attributes['username'].blank? }
+  accepts_nested_attributes_for :user, reject_if: proc { |attributes| attributes['username'] == "" }
 
   def user_attributes=(user_attributes)
     if user_attributes.values[0] != ""
